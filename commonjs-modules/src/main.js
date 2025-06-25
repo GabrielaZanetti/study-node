f36674ed0a7d5526181f39a5371e7f7e1a0e799c
GabrielaZanetti <gabiisctt@gmail.com>
2025-06-25 16:50:52 -0300

    exportar de diferentes formas

--- commonjs-modules/src/main.js
+++ commonjs-modules/src/main.js
@@ -1,12 +1,15 @@
 const { getFullName, getProductLabel } = require("./services/products");
 const config = require("./services/config");
+const database = require("./services/database");
 
 async function main() {
-    // getFullName(123, 'Projeto1')
-    // getFullName(456, 'Projeto2')
-    // getProductLabel('Produto3')
+    getFullName(123, 'Projeto1')
+    getFullName(456, 'Projeto2')
+    getProductLabel('Produto3')
 
     console.log(config.production);
+    
+    database.connnectToDatabase("database")
 }
 
 main()
---
+++ commonjs-modules/src/services/database.js
@@ -0,0 +1,7 @@
+exports.connnectToDatabase = async (dataName) => {
+    console.log("Se conectando ao banco: "+dataName);
+}
+
+exports.disconnnectToDatabase = async (dataName) => {
+    console.log("Se desconectando ao banco: "+dataName);
+}
